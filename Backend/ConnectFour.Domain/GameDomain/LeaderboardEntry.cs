12fd04f6d20887ae78341139bf8e3ac23305149e
Driesjuhhhh <101977453+Driesjuhhhh@users.noreply.github.com>
2023-06-01 20:42:34 +0200

    CONNECT4 PUSH

---
+++ Backend/ConnectFour.Domain/GameDomain/LeaderboardEntry.cs
@@ -0,0 +1,22 @@
+﻿using ConnectFour.Domain.GameDomain.Contracts;
+using System.Text.Json.Serialization;
+
+namespace ConnectFour.Domain.GameDomain;
+
+/// <inheritdoc cref="ILeaderboardEntry" />
+public class LeaderboardEntry : ILeaderboardEntry
+{
+    public Guid UserId { get; init; }
+
+    public string Username { get; init; }
+
+    [JsonInclude]
+    public int Wins { get; private set; }
+
+    [JsonInclude]
+    public int Losses { get; private set; }
+
+    public void IncrementWins() => Wins++;
+
+    public void IncrementLosses() => Losses++;
+}
---
+++ Backend/ConnectFour.Domain/GameDomain/Move.cs
@@ -0,0 +1,21 @@
+﻿using ConnectFour.Domain.GameDomain.Contracts;
+using ConnectFour.Domain.GridDomain;
+
+namespace ConnectFour.Domain.GameDomain;
+
+/// <inheritdoc cref="IMove"/>
+public class Move : IMove
+{
+    public Move(int column, MoveType type = MoveType.SlideIn, DiscType discType = DiscType.Normal)
+    {
+        Column = column;
+        Type = type;
+        DiscType = discType;
+    }
+
+    public MoveType Type { get; }
+
+    public DiscType DiscType { get; }
+
+    public int Column { get; }
+}
